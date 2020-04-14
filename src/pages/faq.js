a5ef9b750c16506aa67e6083b9a87682a0cc1edf
Alli Black <black.alli@gmail.com>
2020-04-14 08:50:16 -0700

    alli eds to faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -14,13 +14,9 @@ const FAQ = () => {
         </Heading>
         <Text>
           We plan to form an independent non-profit with dedicated staffing to
-          house this work, in a way that ensures neutrality, transparency, and
-          commitment and accountability to the public. Accountability is
-          critical when working with human subject data and potentially
-          sensitive location data, and is the reason to house this in an
-          independently governed non-profit as opposed to a for-profit company
-          or government agency. But time is of the essence, so we are starting
-          immediately, and will build the org responsibly as we go.
+          house this work. However, given the need to move quickly in response to COVID-19, we are starting
+          immediately with volunteer effort. We will build the organization responsibly as we go, valuing neutrality, transparency, and
+          commitment and accountability to the public.
         </Text>
         <Heading as="h3" sx={{ fontSize: [4], fontWeight: "bold", my: 4 }}>
           How is this related to location-based tracing apps?
@@ -29,11 +25,10 @@ const FAQ = () => {
           Our core thesis is that traditional contact tracing — as performed by
           public health officials conducting extensive interviews with positive
           cases — works well. But we simply don't have the human resources to do
-          it at scale. So, our starting point is to scale up the classic
-          approach, using digital surveys and other web-based technology 
-          to fill in the gaps, rather than invent a new approach. 
-          But as location-tracking apps and approaches mature, 
-          we will explore methods to incorporate this data 
+          it at the scale necessary to combat COVID-19. So, our starting point is to scale up the classic
+          approach, using web-based surveys and innovative approaches to data management, rather than invent a new approach.
+          As location-tracking apps and approaches mature,
+          we will explore methods to incorporate this data
           into our approach.
         </Text>
       </Box>
